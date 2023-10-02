91699a22e534635acf2b59cec6e376a95475876a
EwnJR <98489349+EwnJR@users.noreply.github.com>
2023-10-02 16:12:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+const logo = document.getElementById('logoarianegroup');
+
+logo.addEventListener('click', () => {
+  // Changer la largeur de l'image au clic
+  logo.style.width = '250px'; // Vous pouvez ajuster cette valeur selon vos besoins
+});
